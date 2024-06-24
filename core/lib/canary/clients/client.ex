4b08cc22c3130416f339f166394cbb7b45da7b1f
yujonglee <yujonglee.dev@gmail.com>
2024-06-24 21:42:19 +0900

    add discord in client table

--- core/lib/canary/clients/client.ex
+++ core/lib/canary/clients/client.ex
@@ -10,10 +10,12 @@ defmodule Canary.Clients.Client do
       allow_nil? false
     end
 
-    attribute :type, :atom, constraints: [one_of: [:web]]
+    attribute :type, :atom, constraints: [one_of: [:web, :discord]]
 
     attribute :web_base_url, :string
-    attribute :web_public_key, :string, default: &Ash.UUID.generate/0
+    attribute :web_public_key, :string
+
+    attribute :discord_server_id, :integer
   end
 
   actions do
@@ -29,6 +31,21 @@ defmodule Canary.Clients.Client do
       end
 
       change set_attribute(:type, :web)
+      change set_attribute(:web_base_url, expr(^arg(:web_base_url)))
+      change set_attribute(:web_public_key, &Ash.UUID.generate/0)
+    end
+
+    create :create_discord do
+      argument :account_id, :uuid do
+        allow_nil? false
+      end
+
+      argument :discord_server_id, :integer do
+        allow_nil? false
+      end
+
+      change set_attribute(:type, :discord)
+      change set_attribute(:discord_server_id, expr(^arg(:discord_server_id)))
     end
   end
 
