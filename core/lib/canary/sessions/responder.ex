f1b3d7053cf5611c37cc1c47ac3fd351f226f9f5
yujonglee <yujonglee.dev@gmail.com>
2024-06-26 19:24:56 +0900

    chores

--- core/lib/canary/sessions/responder.ex
+++ core/lib/canary/sessions/responder.ex
@@ -43,7 +43,7 @@ defmodule Canary.Sessions.Responder.LLM do
       docs
       |> Enum.map(& &1.source_url)
       |> Enum.reject(&is_nil/1)
-      |> Enum.map(fn url -> "- #{url}" end)
+      |> Enum.map(fn url -> "- <#{url}>" end)
       |> Enum.join("\n")
 
     messages = [
