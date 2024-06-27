a327bb242f25b9684d75e4beb946ffd975930b8e
yujonglee <yujonglee.dev@gmail.com>
2024-06-27 14:53:15 +0900

    render client details

--- core/lib/canary_web/live/client_live.ex
+++ core/lib/canary_web/live/client_live.ex
@@ -16,6 +16,18 @@ defmodule CanaryWeb.ClientLive do
         Delete
       </button>
     </.content_header>
+
+    <div class="mt-8">
+      <%= case @client.type do %>
+        <% :discord -> %>
+          <div class="flex flex-col gap-2">
+            <.render_disabled_input name="Discord Server ID" value={@client.discord_server_id} />
+            <.render_disabled_input name="Discord Channel ID" value={@client.discord_channel_id} />
+          </div>
+        <% :web -> %>
+          <.render_disabled_input name="Web Base URL" value={@client.web_base_url} />
+      <% end %>
+    </div>
     """
   end
 
@@ -27,6 +39,20 @@ defmodule CanaryWeb.ClientLive do
       |> assign(client: client)
 
     {:ok, socket}
+  end
+
+  attr :name, :string, default: nil
+  attr :value, :string, default: nil
+
+  defp render_disabled_input(assigns) do
+    ~H"""
+    <label class="form-control w-full max-w-xs">
+      <div class="label">
+        <span class="label-text"><%= @name %></span>
+      </div>
+      <input type="text" disabled placeholder={@value} class="input input-bordered w-full max-w-xs" />
+    </label>
+    """
   end
 
   def handle_event("delete", _, socket) do
