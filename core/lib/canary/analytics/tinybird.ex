d3ecf3e59206f539f39f382b33da9727f14188d4
yujonglee <yujonglee.dev@gmail.com>
2024-07-10 22:46:24 +0900

    setup tinybird client

---
+++ core/lib/canary/analytics/tinybird.ex
@@ -0,0 +1,16 @@
+defmodule Canary.Analytics.Tinybird do
+  @datasource "web"
+
+  defp client() do
+    base_url = Application.get_env(:canary, :tinybird) |> Keyword.fetch!(:base_url)
+    api_key = Application.get_env(:canary, :tinybird) |> Keyword.fetch!(:api_key)
+  end
+
+  def event(data) do
+    client()
+    |> Req.post(
+      url: "/v0/events?name=#{@datasource}",
+      json: Map.merge(data, %{"timestamp" => DateTime.to_iso8601(DateTime.utc_now())})
+    )
+  end
+end
