81e97cface42d261e2e6bbb51230dd7d8055748c
yujonglee <yujonglee.dev@gmail.com>
2024-07-11 10:20:48 +0900

    add icon to ask panel

---
+++ js/packages/search/src/icons/user.ts
@@ -0,0 +1,18 @@
+import { LitElement, html } from "lit";
+import { customElement } from "lit/decorators.js";
+
+@customElement("hero-user")
+export class HeroIconUser extends LitElement {
+  render() {
+    return html`${this.svg()}`;
+  }
+
+  svg() {
+    return html`
+      <!-- prettier-ignore -->
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+      </svg>
+    `;
+  }
+}
