930d231871e28c0733daaf1361a7262d5751e284
yujonglee <yujonglee.dev@gmail.com>
2024-07-18 14:26:13 +0900

    fix canary import in starlight plugin

--- js/packages/starlight/index.js
+++ js/packages/starlight/index.js
@@ -9,6 +9,24 @@ const plugin = () => {
             ...(config.components ?? {}),
             Search: "@getcanary/starlight/search.astro",
           },
+          head: [
+            ...[
+              "canary-provider-pagefind",
+              "canary-styles-starlight",
+              "canary-modal",
+              "canary-trigger-searchbar",
+              "canary-content",
+              "canary-search",
+              "canary-search-input",
+              "canary-search-results",
+            ].map((c) => ({
+              tag: "script",
+              attrs: {
+                type: "module",
+                src: `https://unpkg.com/@getcanary/web@0.0.31/components/${c}.js`,
+              },
+            })),
+          ],
         });
       },
     },
