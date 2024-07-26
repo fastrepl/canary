a0ef03f2b4e317a154e250772c9272be2c1a873d
yujonglee <yujonglee.dev@gmail.com>
2024-07-26 16:12:42 +0900

    add indexOnly option in docusaurus plugin

--- js/packages/docusaurus-pagefind/src/index.js
+++ js/packages/docusaurus-pagefind/src/index.js
@@ -4,17 +4,27 @@ import { getFilePaths } from "./utils";
 import { buildIndex } from "./pagefind";
 
 export default function plugin(_context, options) {
-  return {
+  const { indexOnly = false } = options;
+
+  const config = {
     name: "docusaurus-plugin-pagefind-canary",
-    getThemePath() {
-      return path.resolve(__dirname, "./theme");
-    },
     async contentLoaded({ actions }) {
       actions.setGlobalData({ options });
     },
     async postBuild({ routesPaths = [], outDir, baseUrl }) {
       const docs = getFilePaths(routesPaths, outDir, baseUrl, options);
       await buildIndex(outDir, docs);
+    },
+  };
+
+  if (indexOnly) {
+    return config;
+  }
+
+  return {
+    ...config,
+    getThemePath() {
+      return path.resolve(__dirname, "./theme");
     },
   };
 }
