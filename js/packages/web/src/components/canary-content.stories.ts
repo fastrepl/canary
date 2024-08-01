cb634464f8215fa6d07cb197ae6a2dd68aeca874
yujonglee <yujonglee.dev@gmail.com>
2024-08-01 17:55:46 +0900

    refactor content story

--- js/packages/web/src/components/canary-content.stories.ts
+++ js/packages/web/src/components/canary-content.stories.ts
@@ -3,7 +3,7 @@ import type { Meta, StoryObj } from "@storybook/web-components";
 import { http, HttpResponse } from "msw";
 
 import { userEvent } from "@storybook/test";
-import * as shadow from "shadow-dom-testing-library";
+import { getByShadowRole } from "shadow-dom-testing-library";
 
 import "./canary-provider-cloud";
 import "./canary-content";
@@ -20,183 +20,133 @@ import "./canary-search-results-tabs";
 import "./canary-callout-cal";
 import "./canary-callout-discord";
 
-const render = ({ type }: any) => {
-  if (type === "search") {
-    return html`
-      <canary-provider-cloud endpoint="http://localhost:6006" key="key">
-        <canary-content>
-          <canary-search slot="search">
-            <canary-search-input slot="input"></canary-search-input>
-            <canary-search-results slot="results"></canary-search-results>
-          </canary-search>
-        </canary-content>
-      </canary-provider-cloud>
-    `;
-  }
+enum Kind {
+  Search,
+  SearchEmpty,
+  SearchGroup,
+  SearchTabs,
+  SearchError,
+  SearchCallout,
+  SearchMobile,
+}
 
-  if (type === "search-group") {
-    return html`
-      <canary-provider-cloud endpoint="http://localhost:6006" key="key">
-        <canary-content query="hi">
-          <canary-search slot="search">
-            <canary-search-input slot="input"></canary-search-input>
-            <canary-search-results slot="results" group></canary-search-results>
-            <canary-callout-discord slot="callout"></canary-callout-discord>
-          </canary-search>
-        </canary-content>
-      </canary-provider-cloud>
-    `;
-  }
-
-  if (type === "search-tabs") {
-    return html`
-      <canary-provider-cloud endpoint="http://localhost:6006" key="key">
-        <canary-content query="hi">
-          <canary-search slot="search">
-            <canary-search-input slot="input"></canary-search-input>
-            <canary-search-results-tabs
-              slot="results"
-              group
-              tabs="Docs:*;API:/api/.+$"
-            ></canary-search-results-tabs>
-            <canary-callout-discord slot="callout"></canary-callout-discord>
-          </canary-search>
-        </canary-content>
-      </canary-provider-cloud>
-    `;
-  }
-
-  if (type === "search-callout") {
-    return html`
-      <canary-provider-cloud endpoint="http://localhost:6006" key="key">
-        <canary-content query="discord">
-          <canary-search slot="search">
-            <canary-search-input slot="input"></canary-search-input>
-            <canary-search-results slot="results"></canary-search-results>
-            <canary-callout-discord slot="callout"></canary-callout-discord>
-          </canary-search>
-        </canary-content>
-      </canary-provider-cloud>
-    `;
-  }
-
-  if (type === "search-error") {
-    return html`
-      <canary-provider-cloud endpoint="http://localhost:6006" key="key">
-        <canary-content query="hi">
-          <canary-search slot="search">
-            <canary-search-input slot="input"></canary-search-input>
-            <canary-search-results slot="results"></canary-search-results>
-          </canary-search>
-        </canary-content>
-      </canary-provider-cloud>
-    `;
-  }
-
-  if (type === "ask") {
-    return html`
-      <canary-provider-cloud endpoint="http://localhost:6006" key="key">
-        <canary-content>
-          <canary-ask slot="ask">
-            <canary-ask-input slot="input"></canary-ask-input>
-            <canary-ask-results slot="results"></canary-ask-results>
-          </canary-ask>
-        </canary-content>
-      </canary-provider-cloud>
-    `;
-  }
-
-  if (type === "search-ask") {
-    return html`
-      <canary-provider-cloud endpoint="http://localhost:6006" key="key">
-        <canary-content query="hi">
-          <canary-search slot="search">
-            <canary-search-input slot="input"></canary-search-input>
-            <canary-search-results slot="results"></canary-search-results>
-          </canary-search>
-          <canary-ask slot="ask">
-            <canary-ask-input slot="input"></canary-ask-input>
-            <canary-ask-results slot="results"></canary-ask-results>
-          </canary-ask>
-        </canary-content>
-      </canary-provider-cloud>
-    `;
-  }
-
-  if (type === "search-ask-error") {
-    return html`
-      <canary-provider-cloud endpoint="http://localhost:6006" key="key">
-        <canary-content query="hi">
-          <canary-search-input slot="search-input"></canary-search-input>
-          <canary-search slot="search"></canary-search>
-          <canary-ask-input slot="ask-input"></canary-ask-input>
-          <canary-ask slot="ask"></canary-ask>
-        </canary-content>
-      </canary-provider-cloud>
-    `;
-  }
-
-  return html`<canary-content></canary-content>`;
-};
-
-const play: StoryObj["play"] = async ({ canvasElement }) => {
-  const input = shadow.getByShadowRole(canvasElement, "textbox");
-  await userEvent.type(input, "hello");
+const type = (text: string): StoryObj["play"] => {
+  return async ({ canvasElement }) => {
+    const input = getByShadowRole(canvasElement, "textbox");
+    await userEvent.type(input, text);
+  };
 };
 
 export default {
-  render,
   title: "Public/canary-content",
-  parameters: {
-    sourceLink: "canary-content.stories.ts",
+  parameters: { sourceLink: "canary-content.stories.ts" },
+  render: ({ kind }: { kind: Kind }) => {
+    const wrapper = (child: any) => html`
+      <canary-provider-cloud endpoint="http://localhost:6006" key="key">
+        <canary-content> ${child} </canary-content>
+      </canary-provider-cloud>
+    `;
+
+    if (kind === Kind.Search) {
+      return wrapper(html`
+        <canary-search slot="search">
+          <canary-search-input slot="input"></canary-search-input>
+          <canary-search-results slot="results"></canary-search-results>
+        </canary-search>
+      `);
+    }
+
+    if (kind === Kind.SearchEmpty) {
+      return wrapper(html`
+        <canary-search slot="search">
+          <canary-search-input slot="input"></canary-search-input>
+          <canary-search-results slot="results"></canary-search-results>
+        </canary-search>
+      `);
+    }
+
+    if (kind === Kind.SearchGroup) {
+      return wrapper(html`
+        <canary-search slot="search">
+          <canary-search-input slot="input"></canary-search-input>
+          <canary-search-results slot="results" group></canary-search-results>
+        </canary-search>
+      `);
+    }
+
+    if (kind === Kind.SearchTabs) {
+      return wrapper(html`
+        <canary-search slot="search">
+          <canary-search-input slot="input"></canary-search-input>
+          <canary-search-results-tabs
+            slot="results"
+            group
+            tabs="Docs:*;API:/api/.+$"
+          ></canary-search-results-tabs>
+          <canary-callout-discord slot="callout"></canary-callout-discord>
+        </canary-search>
+      `);
+    }
+
+    if (kind === Kind.SearchCallout) {
+      return wrapper(html`
+        <canary-search slot="search">
+          <canary-search-input slot="input"></canary-search-input>
+          <canary-search-results slot="results"></canary-search-results>
+          <canary-callout-discord slot="callout"></canary-callout-discord>
+        </canary-search>
+      `);
+    }
+
+    if (kind === Kind.SearchError) {
+      return wrapper(html`
+        <canary-search slot="search">
+          <canary-search-input slot="input"></canary-search-input>
+          <canary-search-results slot="results"></canary-search-results>
+        </canary-search>
+      `);
+    }
+
+    if (kind === Kind.SearchMobile) {
+      return wrapper(html`
+        <canary-search slot="search">
+          <canary-search-input slot="input"></canary-search-input>
+          <canary-search-results slot="results"></canary-search-results>
+        </canary-search>
+      `);
+    }
+
+    throw new Error();
   },
-} satisfies Meta;
+} satisfies Meta<{ kind: Kind }>;
 
 export const Search: StoryObj = {
-  args: { type: "search" },
-  play,
+  args: { kind: Kind.Search },
+  play: type("hi"),
+};
+
+export const SearchEmpty: StoryObj = {
+  args: { kind: Kind.SearchEmpty },
 };
 
 export const SearchWithGroup: StoryObj = {
-  args: { type: "search-group" },
+  args: { kind: Kind.SearchGroup },
+  play: type("hi"),
 };
 
 export const SearchWithTabs: StoryObj = {
-  args: { type: "search-tabs" },
-};
-
-export const SearchError: StoryObj = {
-  args: { type: "search-error" },
-  parameters: {
-    msw: {
-      handlers: {
-        search: http.post("/api/v1/search", async () => {
-          return HttpResponse.json([], { status: 429 });
-        }),
-      },
-    },
-  },
+  args: { kind: Kind.SearchTabs },
+  play: type("hi"),
 };
 
 export const SearchCallout: StoryObj = {
-  args: { type: "search-callout" },
+  args: { kind: Kind.SearchCallout },
+  play: type("discord"),
 };
 
-export const SearchMobile: StoryObj = {
-  args: { type: "search" },
-  parameters: { viewport: { defaultViewport: "mobile1", disable: false } },
-};
-
-export const Ask: StoryObj = {
-  args: { type: "ask" },
-};
-
-export const SearchAsk: StoryObj = {
-  args: { type: "search-ask" },
-};
-
-export const SearchAskError: StoryObj = {
-  args: { type: "search-ask-error" },
+export const SearchError: StoryObj = {
+  args: { kind: Kind.SearchError },
+  play: type("hi"),
   parameters: {
     msw: {
       handlers: {
@@ -206,4 +156,10 @@ export const SearchAskError: StoryObj = {
       },
     },
   },
+};
+
+export const SearchMobile: StoryObj = {
+  args: { kind: Kind.SearchMobile },
+  play: type("hi"),
+  parameters: { viewport: { defaultViewport: "mobile1", disable: false } },
 };
