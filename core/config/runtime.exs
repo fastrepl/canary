dbcd5fc44366d91c0bee6978da85f6990ceffffc
yujonglee <yujonglee.dev@gmail.com>
2024-08-20 21:46:52 +0900

    fix ssl

--- core/config/runtime.exs
+++ core/config/runtime.exs
@@ -31,10 +31,10 @@ if config_env() == :prod do
   maybe_ipv6 = if System.get_env("ECTO_IPV6") in ~w(true 1), do: [:inet6], else: []
 
   config :canary, Canary.Repo,
-    # ssl: true,
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-    socket_options: maybe_ipv6
+    socket_options: maybe_ipv6,
+    ssl: [cacerts: :public_key.cacerts_get()]
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
   # A default value is used in config/dev.exs and config/test.exs but you
