af9bd0d6194044229008f9c0cdedf253a652b128
yujonglee <yujonglee.dev@gmail.com>
2024-09-04 14:09:57 +0900

    add subdomain router and liveview

--- core/lib/canary_web/controllers/experimental_controller.ex
+++
@@ -1,9 +0,0 @@
-defmodule CanaryWeb.ExperimentalController do
-  use CanaryWeb, :controller
-
-  def embed(conn, _params) do
-    conn
-    |> put_resp_content_type("text/html")
-    |> send_resp(200, "<h1>Hello #{conn.private[:subdomain]}</h1>")
-  end
-end
