d57620dcb1af03e5928b75722e2d27e70cae55cf
yujonglee <yujonglee.dev@gmail.com>
2024-09-10 10:51:45 +0900

    update home_live to provide sources

--- core/lib/canary_web/live/home_live.ex
+++ core/lib/canary_web/live/home_live.ex
@@ -1,46 +1,47 @@
 defmodule CanaryWeb.HomeLive do
   use CanaryWeb, :live_view
 
+  @impl true
   def render(assigns) do
     ~H"""
     <div class="flex flex-col">
       <canary-root>
         <canary-provider-cloud
-          api-key={@web_client.web_public_key}
+          api-key={Enum.at(@current_account.keys, 0).value}
           api-base={CanaryWeb.Endpoint.url()}
+          sources={
+            @current_account.sources
+            |> Enum.map(& &1.name)
+            |> Enum.join(",")
+          }
         >
-          <canary-modal>
-            <canary-trigger-searchbar slot="trigger"></canary-trigger-searchbar>
-            <canary-content slot="content">
-              <canary-search slot="mode">
-                <canary-search-input slot="input" autofocus></canary-search-input>
-                <canary-search-suggestions slot="body" header="Ask AI"></canary-search-suggestions>
-                <canary-search-results slot="body" header="Results" limit="4"></canary-search-results>
-              </canary-search>
-              <canary-ask slot="mode">
-                <canary-mode-breadcrumb slot="input-before" previous="Search" text="Ask AI">
-                </canary-mode-breadcrumb>
-                <canary-ask-input slot="input"></canary-ask-input>
-                <canary-ask-results slot="body"></canary-ask-results>
-              </canary-ask>
-            </canary-content>
-          </canary-modal>
+          <canary-content>
+            <canary-search slot="mode">
+              <canary-search-input slot="input" autofocus></canary-search-input>
+              <canary-search-suggestions slot="body" header="Ask AI"></canary-search-suggestions>
+              <canary-search-results slot="body" header="Results"></canary-search-results>
+            </canary-search>
+            <canary-ask slot="mode">
+              <canary-mode-breadcrumb slot="input-before" text="Ask AI"></canary-mode-breadcrumb>
+              <canary-ask-input slot="input"></canary-ask-input>
+              <canary-ask-results slot="body"></canary-ask-results>
+            </canary-ask>
+          </canary-content>
         </canary-provider-cloud>
       </canary-root>
     </div>
     """
   end
 
+  @impl true
   def mount(_params, _session, socket) do
-    key =
+    account =
       socket.assigns.current_account
-      |> Ash.load!([:keys])
-      |> Map.get(:keys)
-      |> Enum.at(0)
+      |> Ash.load!([:keys, :sources])
 
     socket =
       socket
-      |> assign(web_client: %{web_public_key: key.value})
+      |> assign(current_account: account)
 
     {:ok, socket}
   end
