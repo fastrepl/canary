6062d954fe9fae6c2c85c9dbae17d5ce20c692d7
yujonglee <yujonglee.dev@gmail.com>
2024-09-12 08:37:37 +0900

    refactor document structs

--- core/lib/canary/index/document.ex
+++ core/lib/canary/index/document.ex
@@ -1,49 +1,43 @@
 defmodule Canary.Index.Document.Webpage do
-  @derive Jason.Encoder
-  defstruct [
+  @common [
     :id,
     :source_id,
     :embedding,
     :tags,
-    :meta,
-    #
-    :title,
-    :content
+    :meta
   ]
+
+  @derive Jason.Encoder
+  defstruct @common ++ [:title, :content]
 end
 
 defmodule Canary.Index.Document.Webpage.Meta do
   @derive Jason.Encoder
-  defstruct [
-    :url,
-    :titles
-  ]
+  defstruct [:url, :titles]
 end
 
 defmodule Canary.Index.Document.GithubIssue do
-  @derive Jason.Encoder
-  defstruct [
+  @common [
     :id,
     :source_id,
     :embedding,
     :tags,
-    :meta,
-    #
-    :title,
-    :content
+    :meta
   ]
+
+  @derive Jason.Encoder
+  defstruct @common ++ [:title, :content]
 end
 
 defmodule Canary.Index.Document.GithubDiscussion do
-  @derive Jason.Encoder
-  defstruct [
+  @common [
     :id,
     :source_id,
     :embedding,
     :tags,
-    :meta,
-    #
-    :title,
-    :content
+    :meta
   ]
+
+  @derive Jason.Encoder
+  defstruct @common ++ [:title, :content]
 end
