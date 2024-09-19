31ee0d9b4e81f1534b8449fc24f2b388e248829a
yujonglee <yujonglee.dev@gmail.com>
2024-09-20 08:18:00 +0900

    update deps

--- js/packages/web/.storybook/main.ts
+++ js/packages/web/.storybook/main.ts
@@ -15,7 +15,6 @@ const config: StorybookConfig = {
     getAbsolutePath("@storybook/addon-viewport"),
     getAbsolutePath("@storybook/addon-actions"),
     getAbsolutePath("@storybook/addon-interactions"),
-    getAbsolutePath("@chromatic-com/storybook"),
   ],
   framework: {
     name: getAbsolutePath("@storybook/web-components-vite"),
