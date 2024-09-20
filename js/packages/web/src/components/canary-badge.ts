e7ae92b9dca25b30ebcdbf06fcda536363190e42
yujonglee <yujonglee.dev@gmail.com>
2024-09-20 20:09:36 +0900

    update badge style

--- js/packages/web/src/components/canary-badge.ts
+++ js/packages/web/src/components/canary-badge.ts
@@ -17,8 +17,8 @@ export class CanaryBadge extends LitElement {
     wrapper,
     css`
       .container {
-        padding: 0.15em 0.3em;
-        border-radius: 0.25em;
+        padding: 2px 4px;
+        border-radius: 2px;
         font-size: 0.625rem;
         border: 1px solid var(--canary-color-primary-30);
         color: var(--canary-color-primary-30);
