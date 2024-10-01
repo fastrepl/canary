a83fbf4e46e84eb64817e1e4ae2a1935a0aee296
yujonglee <yujonglee.dev@gmail.com>
2024-10-01 17:49:03 +0900

    output cem in build step

--- js/packages/web/vite.config.ts
+++ js/packages/web/vite.config.ts
@@ -4,9 +4,47 @@ import { resolve } from "path";
 
 import { defineConfig } from "vite";
 import unocss from "unocss/vite";
-import dts from "vite-plugin-dts";
 
+import dts from "vite-plugin-dts";
+import cem from "vite-plugin-cem";
 import { partsReportPlugin, cssVariablesReportPlugin } from "./plugins";
+
+const components = [
+  ...["canary-root", "canary-styles"],
+  ...[
+    "canary-provider-mock",
+    "canary-provider-cloud",
+    "canary-provider-pagefind",
+    "canary-provider-vitepress-minisearch",
+  ],
+  ...[
+    "canary-callout",
+    "canary-callout-cal",
+    "canary-callout-calendly",
+    "canary-callout-slack",
+    "canary-callout-discord",
+  ],
+  ...[
+    "canary-modal",
+    "canary-content",
+    "canary-trigger-searchbar",
+    "canary-trigger-logo",
+  ],
+  "canary-input",
+  "canary-search",
+  "canary-search-results",
+  "canary-search-match",
+  "canary-search-suggestions",
+  "canary-search-empty",
+  "canary-ask",
+  "canary-ask-results",
+  "canary-tooltip",
+  "canary-footer",
+  "canary-media-query",
+  "canary-mode-tabs",
+  "canary-mode-breadcrumb",
+  "canary-filter-tabs-glob",
+].map((name) => `components/${name}`);
 
 export default defineConfig({
   test: {
@@ -17,6 +55,12 @@ export default defineConfig({
     process.env.GENERATE_DOCS === "1" && cssVariablesReportPlugin(),
     unocss({ mode: "shadow-dom" }),
     dts({ exclude: ["**/*.stories.*"] }),
+    cem({
+      files: components,
+      output: "custom-elements.json",
+      lit: true,
+      packageJson: true,
+    }),
   ].filter(Boolean),
   build: {
     minify: "terser",
@@ -27,42 +71,7 @@ export default defineConfig({
     },
     lib: {
       entry: [
-        ...[
-          ...["canary-root", "canary-styles"],
-          ...[
-            "canary-provider-mock",
-            "canary-provider-cloud",
-            "canary-provider-pagefind",
-            "canary-provider-vitepress-minisearch",
-          ],
-          ...[
-            "canary-callout",
-            "canary-callout-cal",
-            "canary-callout-calendly",
-            "canary-callout-slack",
-            "canary-callout-discord",
-          ],
-          ...[
-            "canary-modal",
-            "canary-content",
-            "canary-trigger-searchbar",
-            "canary-trigger-logo",
-          ],
-          "canary-input",
-          "canary-search",
-          "canary-search-results",
-          "canary-search-match",
-          "canary-search-suggestions",
-          "canary-search-empty",
-          "canary-ask",
-          "canary-ask-results",
-          "canary-tooltip",
-          "canary-footer",
-          "canary-media-query",
-          "canary-mode-tabs",
-          "canary-mode-breadcrumb",
-          "canary-filter-tabs-glob",
-        ].map((name) => `components/${name}`),
+        ...components,
         ...["contexts", "controllers", "types/index", "utils", "store/index"],
       ].reduce(
         (acc, cur) => ({ ...acc, [cur]: resolve(__dirname, `src/${cur}.ts`) }),
