418a5fd689d1226d9d219d657faa37b0998fc810
yujonglee <yujonglee.dev@gmail.com>
2024-10-07 16:56:24 +0900

    optimize documents creation with batching

--- core/lib/canary/sources/document/create_github_discussion.ex
+++ core/lib/canary/sources/document/create_github_discussion.ex
@@ -1,7 +1,6 @@
 defmodule Canary.Sources.Document.CreateGithubDiscussion do
   use Ash.Resource.Change
 
-  alias Canary.Sources.Document
   alias Canary.Sources.GithubDiscussion
 
   @impl true
@@ -20,26 +19,27 @@ defmodule Canary.Sources.Document.CreateGithubDiscussion do
   end
 
   @impl true
-  def change(changeset, opts, _context) do
-    source_id = Ash.Changeset.get_argument(changeset, opts[:source_id_argument])
-    fetcher_results = Ash.Changeset.get_argument(changeset, opts[:fetcher_results_argument])
+  def before_batch(changesets, opts, _context) do
+    changesets =
+      changesets
+      |> Enum.map(fn changeset ->
+        changeset
+        |> Ash.Changeset.force_change_attribute(:id, Ash.UUID.generate())
+      end)
 
-    changeset
-    |> Ash.Changeset.change_attribute(
-      opts[:meta_attribute],
-      wrap_union(%GithubDiscussion.DocumentMeta{})
-    )
-    |> Ash.Changeset.change_attribute(opts[:chunks_attribute], [])
-    |> Ash.Changeset.after_action(fn _, record ->
-      top_level_item = fetcher_results |> Enum.at(0)
+    chunk_inputs =
+      changesets
+      |> Enum.flat_map(fn changeset ->
+        document_id = Ash.Changeset.get_attribute(changeset, :id)
+        source_id = Ash.Changeset.get_argument(changeset, opts[:source_id_argument])
+        fetcher_results = Ash.Changeset.get_argument(changeset, opts[:fetcher_results_argument])
 
-      chunks_create_result =
         fetcher_results
         |> Enum.with_index(0)
         |> Enum.map(fn {%GithubDiscussion.FetcherResult{} = item, index} ->
           %{
             source_id: source_id,
-            document_id: record.id,
+            document_id: document_id,
             node_id: item.node_id,
             is_parent: index == 0,
             title: item.title,
@@ -51,31 +51,60 @@ defmodule Canary.Sources.Document.CreateGithubDiscussion do
             comment: item.comment
           }
         end)
-        |> Ash.bulk_create(GithubDiscussion.Chunk, :create,
-          return_errors?: true,
-          return_records?: true
-        )
+      end)
 
-      meta = %{
-        title: top_level_item.title,
-        url: top_level_item.url,
-        closed: top_level_item.closed,
-        answered: top_level_item.answered
-      }
+    bulk_opts = [return_errors?: true, return_records?: true, batch_size: 100]
 
-      case chunks_create_result do
-        %Ash.BulkResult{status: :success, records: records} ->
-          case Document.update(record, wrap_union(meta), Enum.map(records, &wrap_union/1)) do
-            {:ok, updated_record} -> {:ok, updated_record}
-            error -> error
-          end
+    case Ash.bulk_create(chunk_inputs, GithubDiscussion.Chunk, :create, bulk_opts) do
+      %Ash.BulkResult{status: :success, records: records} ->
+        doc_id_to_chunks =
+          records
+          |> Enum.group_by(& &1.document_id)
 
-        %Ash.BulkResult{errors: errors} ->
-          {:error, errors}
-      end
+        changesets
+        |> Enum.map(fn changeset ->
+          document_id = Ash.Changeset.get_attribute(changeset, :id)
+          chunks = doc_id_to_chunks[document_id] || []
+
+          changeset
+          |> Ash.Changeset.change_attribute(opts[:chunks_attribute], wrap_union(chunks))
+        end)
+
+      %Ash.BulkResult{errors: errors} ->
+        changesets
+        |> Enum.map(fn changeset ->
+          changeset
+          |> Ash.Changeset.add_error(errors)
+          |> Ash.Changeset.change_attribute(opts[:chunks_attribute], [])
+        end)
+    end
+  end
+
+  @impl true
+  def batch_change(changesets, opts, _context) do
+    changesets
+    |> Enum.map(fn changeset ->
+      fetcher_results = Ash.Changeset.get_argument(changeset, opts[:fetcher_results_argument])
+
+      top_level_item = fetcher_results |> Enum.at(0)
+
+      meta =
+        GithubDiscussion.DocumentMeta
+        |> Ash.Changeset.for_create(:create, %{
+          title: top_level_item.title,
+          url: top_level_item.url,
+          closed: top_level_item.closed,
+          answered: top_level_item.answered
+        })
+        |> Ash.create!()
+        |> then(&wrap_union/1)
+
+      changeset
+      |> Ash.Changeset.change_attribute(opts[:meta_attribute], meta)
     end)
   end
 
+  defp wrap_union(v) when is_list(v), do: Enum.map(v, &wrap_union/1)
   defp wrap_union(%Ash.Union{} = v), do: v
   defp wrap_union(v), do: %Ash.Union{type: :github_discussion, value: v}
 end
--- core/lib/canary/sources/document/create_webpage.ex
+++ core/lib/canary/sources/document/create_webpage.ex
@@ -1,7 +1,6 @@
 defmodule Canary.Sources.Document.CreateWebpage do
   use Ash.Resource.Change
 
-  alias Canary.Sources.Document
   alias Canary.Sources.Webpage
 
   @impl true
@@ -20,29 +19,29 @@ defmodule Canary.Sources.Document.CreateWebpage do
   end
 
   @impl true
-  def change(changeset, opts, _context) do
-    source_id = Ash.Changeset.get_argument(changeset, opts[:source_id_argument])
-    fetcher_result = Ash.Changeset.get_argument(changeset, opts[:fetcher_result_argument])
+  def before_batch(changesets, opts, _context) do
+    changesets =
+      changesets
+      |> Enum.map(fn changeset ->
+        changeset
+        |> Ash.Changeset.force_change_attribute(:id, Ash.UUID.generate())
+      end)
 
-    changeset
-    |> Ash.Changeset.change_attribute(opts[:meta_attribute], wrap_union(%Webpage.DocumentMeta{}))
-    |> Ash.Changeset.change_attribute(opts[:chunks_attribute], [])
-    |> Ash.Changeset.after_action(fn _, record ->
-      %Webpage.FetcherResult{url: url, html: html, items: items, tags: tags} = fetcher_result
-      top_level_item = items |> Enum.at(0)
+    chunk_inputs =
+      changesets
+      |> Enum.flat_map(fn changeset ->
+        document_id = Ash.Changeset.get_attribute(changeset, :id)
+        source_id = Ash.Changeset.get_argument(changeset, opts[:source_id_argument])
+        fetcher_result = Ash.Changeset.get_argument(changeset, opts[:fetcher_result_argument])
 
-      hash =
-        html
-        |> then(&:crypto.hash(:sha256, &1))
-        |> Base.encode16(case: :lower)
+        %Webpage.FetcherResult{url: url, items: items, tags: tags} = fetcher_result
 
-      chunks_create_result =
         items
         |> Enum.with_index(0)
         |> Enum.map(fn {%Canary.Scraper.Item{} = item, index} ->
           %{
             source_id: source_id,
-            document_id: record.id,
+            document_id: document_id,
             is_parent: index == 0,
             title: item.title,
             content: item.content,
@@ -50,30 +49,60 @@ defmodule Canary.Sources.Document.CreateWebpage do
             url: URI.parse(url) |> Map.put(:fragment, item.id) |> to_string()
           }
         end)
-        |> Ash.bulk_create(Webpage.Chunk, :create,
-          return_errors?: true,
-          return_records?: true
-        )
+      end)
 
-      meta = %{
-        title: top_level_item.title,
-        url: url,
-        hash: hash
-      }
+    bulk_opts = [return_errors?: true, return_records?: true, batch_size: 100]
 
-      case chunks_create_result do
-        %Ash.BulkResult{status: :success, records: records} ->
-          case Document.update(record, wrap_union(meta), Enum.map(records, &wrap_union/1)) do
-            {:ok, updated_record} -> {:ok, updated_record}
-            error -> error
-          end
+    case Ash.bulk_create(chunk_inputs, Webpage.Chunk, :create, bulk_opts) do
+      %Ash.BulkResult{status: :success, records: records} ->
+        doc_id_to_chunks =
+          records
+          |> Enum.group_by(& &1.document_id)
 
-        %Ash.BulkResult{errors: errors} ->
-          {:error, errors}
-      end
+        changesets
+        |> Enum.map(fn changeset ->
+          document_id = Ash.Changeset.get_attribute(changeset, :id)
+          chunks = doc_id_to_chunks[document_id] || []
+
+          changeset
+          |> Ash.Changeset.change_attribute(opts[:chunks_attribute], wrap_union(chunks))
+        end)
+
+      %Ash.BulkResult{errors: errors} ->
+        changesets
+        |> Enum.map(fn changeset ->
+          changeset
+          |> Ash.Changeset.add_error(errors)
+          |> Ash.Changeset.change_attribute(opts[:chunks_attribute], [])
+        end)
+    end
+  end
+
+  @impl true
+  def batch_change(changesets, opts, _context) do
+    changesets
+    |> Enum.map(fn changeset ->
+      fetcher_result = Ash.Changeset.get_argument(changeset, opts[:fetcher_result_argument])
+      %Webpage.FetcherResult{url: url, html: html, items: items} = fetcher_result
+      top_level_item = items |> Enum.at(0)
+
+      hash =
+        html
+        |> then(&:crypto.hash(:sha256, &1))
+        |> Base.encode16(case: :lower)
+
+      meta =
+        Webpage.DocumentMeta
+        |> Ash.Changeset.for_create(:create, %{title: top_level_item.title, url: url, hash: hash})
+        |> Ash.create!()
+        |> then(&wrap_union/1)
+
+      changeset
+      |> Ash.Changeset.change_attribute(opts[:meta_attribute], meta)
     end)
   end
 
+  defp wrap_union(v) when is_list(v), do: Enum.map(v, &wrap_union/1)
   defp wrap_union(%Ash.Union{} = v), do: v
   defp wrap_union(v), do: %Ash.Union{type: :webpage, value: v}
 end
