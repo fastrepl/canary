8ddd8036f38048044b1eaabe20abd0e61a500cc2
yujonglee <yujonglee.dev@gmail.com>
2024-10-08 19:13:26 +0900

    check last_fetched_at in source processor

--- core/lib/canary/workers/source_processor.ex
+++ core/lib/canary/workers/source_processor.ex
@@ -18,10 +18,14 @@ defmodule Canary.Workers.SourceProcessor do
   def perform(%Oban.Job{}) do
     sources =
       Source
-      |> Ash.Query.select([:id, :config])
+      |> Ash.Query.select([:id, :config, :last_fetched_at])
       |> Ash.read!()
 
     sources
+    |> Enum.filter(fn
+      %Source{last_fetched_at: nil} -> false
+      %Source{last_fetched_at: time} -> DateTime.diff(DateTime.utc_now(), time, :hour) > 18
+    end)
     |> Enum.map(fn %Source{id: id, config: %Ash.Union{type: type}} ->
       case type do
         :webpage -> Workers.WebpageProcessor.new(%{source_id: id})
