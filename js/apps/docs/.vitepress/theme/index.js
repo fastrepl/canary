44b879a3963d489077ceeede638169798eace4fa
yujonglee <yujonglee.dev@gmail.com>
2024-10-08 11:31:07 +0900

    add header highlight in docs

--- js/apps/docs/.vitepress/theme/index.js
+++ js/apps/docs/.vitepress/theme/index.js
@@ -7,6 +7,9 @@ import "./global.css";
 import { inject } from "@vercel/analytics";
 import CloudSearch from "../../components/CloudSearch.vue";
 
+import { NolebaseHighlightTargetedHeading } from "@nolebase/vitepress-plugin-highlight-targeted-heading/client";
+import "@nolebase/vitepress-plugin-highlight-targeted-heading/client/style.css";
+
 /** @type {import('vitepress').Theme} */
 export default {
   extends: DefaultTheme,
@@ -21,6 +24,7 @@ export default {
 
     return h(DefaultTheme.Layout, null, {
       "nav-bar-content-before": () => (show ? h(CloudSearch) : null),
+      "layout-top": () => h(NolebaseHighlightTargetedHeading),
     });
   },
 };
