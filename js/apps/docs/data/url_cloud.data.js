8adeccf3455d4df28f4dba41ad03f46d9f73c63b
yujonglee <yujonglee.dev@gmail.com>
2024-10-08 11:04:59 +0900

    fetch api-base and api-key from env in docs

--- js/apps/docs/data/url_cloud.data.js
+++ js/apps/docs/data/url_cloud.data.js
@@ -1,13 +1,31 @@
+import fs from "node:fs";
+import { parse } from "dotenv";
+
 export default {
-  async load() {
-    return process.env.NODE_ENV === "production"
-      ? {
-          base: "https://cloud.getcanary.dev",
-          key: process.env.CANARY_API_KEY,
-        }
-      : {
-          base: "https://cloud.getcanary.dev",
-          key: "uHH8gCOBQxBW_LYBq59DtGetN9ivlNo-",
-        };
+  watch: ["../.env"],
+  load([envPath]) {
+    if (process.env.NODE_ENV === "production") {
+      return forProd();
+    } else {
+      return forDev(envPath);
+    }
   },
+};
+
+const forDev = (path) => {
+  const { CANARY_API_BASE, CANARY_API_KEY } = parse(
+    fs.readFileSync(path, "utf-8"),
+  );
+
+  return {
+    base: CANARY_API_BASE,
+    key: CANARY_API_KEY,
+  };
+};
+
+const forProd = () => {
+  return {
+    base: process.env.CANARY_API_BASE,
+    key: process.env.CANARY_API_KEY,
+  };
 };
