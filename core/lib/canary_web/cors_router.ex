a5727e964e8673038745f54ee5d26ee760df4724
yujonglee <yujonglee.dev@gmail.com>
2024-10-10 08:42:38 +0900

    allow any origin for api route

--- core/lib/canary_web/cors_router.ex
+++ core/lib/canary_web/cors_router.ex
@@ -1,21 +1,10 @@
 defmodule CanaryWeb.CORSRouter do
   use Corsica.Router,
-    max_age: 300,
     allow_credentials: false,
     allow_headers: ["Authorization", "Content-Type"],
     origins: {CanaryWeb.CORSRouter, :is_allowed_origin?, []}
 
-  def is_allowed_origin?(_conn, origin) do
-    %URI{host: host} = URI.parse(origin)
-    host in hosts()
-  end
-
-  defp hosts() do
-    Cachex.fetch!(:cache, :origin, fn _ ->
-      hosts = Canary.Accounts.Key.allowed_hosts!()
-      {:commit, hosts, expire: :timer.seconds(15)}
-    end)
-  end
+  def is_allowed_origin?(_conn, _origin), do: true
 
   resource "/api/v1/*"
 end
