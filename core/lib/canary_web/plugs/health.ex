0868df66bf8adb63614ddc1399f1335933d7c063
yujonglee <yujonglee.dev@gmail.com>
2024-10-15 09:41:32 +0900

    fly bluegreen deployment

--- core/lib/canary_web/plugs/health.ex
+++ core/lib/canary_web/plugs/health.ex
@@ -4,10 +4,22 @@ defmodule CanaryWeb.Plug.Health do
   def init(opts), do: opts
 
   def call(%Plug.Conn{request_path: "/health"} = conn, _opts) do
-    conn
-    |> send_resp(200, "OK")
-    |> halt()
+    [db?()]
+    |> Enum.all?()
+    |> case do
+      true -> conn |> send_resp(200, "") |> halt()
+      false -> conn |> send_resp(500, "") |> halt()
+    end
   end
 
   def call(conn, _opts), do: conn
+
+  defp db?() do
+    try do
+      Ecto.Adapters.SQL.query(Canary.Repo, "select 1", [])
+      :ok
+    rescue
+      DBConnection.ConnectionError -> :error
+    end
+  end
 end
